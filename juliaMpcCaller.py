0b6d3a807b94be54b349ce3925f4d37baa0eb345
Frank Chuang <fc4321@gmail.com>
2015-10-20 23:26:21 -0700

    add Python files to talk with Julia files

---
+++ juliaMpcCaller.py
@@ -0,0 +1,7 @@
+import julia
+import sys
+import h5py
+
+matricesCooling = h5py.File('matricesCooling.hdf5','r')
+matricesHeating = h5py.File('matricesHeating.hdf5','r')
+matricesCoasting = h5py.File('matricesCoasting.hdf5','r')
