688743bbfad02ed31307d5111276caf76c88dbba
Frank Chuang <fc4321@gmail.com>
2015-11-05 12:42:09 -0800

    updates to juliaMpcCaller.py

--- juliaMpcCaller.py
+++ juliaMpcCaller.py
@@ -5,3 +5,6 @@ import h5py
 matricesCooling = h5py.File('matricesCooling.hdf5','r')
 matricesHeating = h5py.File('matricesHeating.hdf5','r')
 matricesCoasting = h5py.File('matricesCoasting.hdf5','r')
+
+pyjulia = julia.Julia()
+pyjulia.eval('include("finiteHorizonControllerMultiZone.jl")')
