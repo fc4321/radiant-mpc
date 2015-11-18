9a9459b29d9b0e0fb5323aef53174448a03d1316
Frank Chuang <fc4321@gmail.com>
2015-11-17 17:37:34 -0800

    updates to stepTestCommand.py

--- stepTestCommand.py
+++ stepTestCommand.py
@@ -1,6 +1,7 @@
 import sys
 
 time = sys.argv[1]
+numZones = sys.arg[2]
 
 if time < 3*60*60:
     waterFlowRate = 100
@@ -8,3 +9,9 @@ if time < 3*60*60:
 else:
     waterFlowRate = 0
     waterTemperature = 32
+
+output = ''
+for zone in range(numZones):
+    output = output + str(waterFlowRate) + ' ' + str(waterTemperature) + ' '
+output = output[0:-1]
+sys.stdout.write(output)
