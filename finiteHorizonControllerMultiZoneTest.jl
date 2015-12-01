017c8c5b7b98f89f5ba85a2b9b5db4089ba92fb2
Frank Chuang <fc4321@gmail.com>
2015-12-01 10:19:37 -0800

    changed finite horizon controller test

--- finiteHorizonControllerMultiZoneTest.jl
+++ finiteHorizonControllerMultiZoneTest.jl
@@ -3,14 +3,14 @@ using Cbc
 
 include("finiteHorizonControllerMultiZone.jl")
 
-A = {rand(2,2,3)-0.5, rand(2,2,3)-0.5};
-E = {rand(2,2,3), rand(2,2,3)};
-x0=[1;1];
+A = {1,1,1}
+E = {1,0,-1};
+x0=[2]
 
-H = [eye(2);-eye(2)];
-b = 50*ones(4);
+H = [1;-1];
+b = [5;5];
 
-N=2
-d = rand(2,N,2);
+N= 2;
+d = ones(1,N,1);
 
 modes = finiteHorizonControllerMultiZone(A,E,x0,H,b,d,1,-1,1)
