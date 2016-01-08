7df6cb70f537e60991cfd62039123d4919200e14
Frank Chuang <fc4321@gmail.com>
2016-01-08 14:38:23 -0800

    added flexlab fmu

--- dataGather.py
+++ dataGather.py
@@ -2,6 +2,8 @@ import h5py
 import sys
 
 systemMode = sys.argv[1]
+numZones = sys.argv[2]
+
 if systemMode == 1:
     systemMode = 'Cooling'
 elif systeMode == 2:
@@ -10,27 +12,23 @@ else:
     systemMode = 'Heating'
 
 matricesData = h5py.File('matrices' + systemMode + 'Data.hdf5','a')
-numZones = sys.argv[2]
-newDataX1 = dict()
-newDataX2 = dict()
-newDataD = dict()
-x1 = dict()
-x2 = dict()
-d = dict()
 
-for zone in range(numZones):
-    newDataX1[zone] = sys.argv[3+zone*3]
-    newDataX2[zone] = sys.argv[4+zone*3]
-    newDataD[zone] = sys.argv[5+zone*3]
-    x1[zone] = matricesData['x1' + str(zone)]
-    x2[zone] = matricesData['x2' + str(zone)]
-    d[zone] = matricesData['d' + str(zone)]
-    x1[zone].append(newDataX1[zone])
-    x2[zone].append(newDataX2[zone])
-    d[zone].append(newDataD[zone])
-    dsetx1 = matricesData.create_dataset('x1' + str(zone), (len(x1[zone]),1))
-    dsetx2 = matricesData.create_dataset('x2' + str(zone), (len(x2[zone]),1))
-    dsetd = matricesData.create_dataset('d' + str(zone), (len(d[zone]),1))
-    dsetx1[:] = x1[zone]
-    dsetx2[:] = x2[zone]
-    dsetd[:] = d[zone]
+if not matricesData.keys():
+    for zone in range(numZones):
+        x1 = matricesData.create_dataset('x1' + str(zone), (1,), maxshape=(100000,))
+        x2 = matricesData.create_dataset('x2' + str(zone), (1,), maxshape=(100000,))
+        d = matricesData.create_dataset('d' + str(zone), (1,), maxshape=(100000,))
+        x1[1] = sys.argv[3+zone*3]
+        x2[1] = sys.argv[4+zone*3]
+        d[1] = sys.argv[5+zone*3]
+else:
+    for zone in range(numZones):
+        x1 = matricesData['x1' + str(zone)]
+        x2 = matricesData['x2' + str(zone)]
+        d = matricesData['d' + str(zone)]
+        x1.resize(len(x1)+1)
+        x2.resize(len(x2)+1)
+        d.resize(len(d)+1)
+        x1[-1] = sys.argv[3+zone*3]
+        x2[-1] = sys.argv[4+zone*3]
+        d[-1] = sys.argv[5+zone*3]
