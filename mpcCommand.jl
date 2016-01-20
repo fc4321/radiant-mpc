408214c67315ef3db167b5b02ab99757b88f69d0
Frank Chuang <fc4321@gmail.com>
2016-01-20 13:06:09 -0800

    working version of closedloop

--- mpcCommand.jl
+++ mpcCommand.jl
@@ -1,13 +1,13 @@
 using HDF5
 
-numZones = parse(Int, ARGS[1])
+numZones = parse(Int, ARGS[1]);
 
-output = h5read("mpcOutput.hdf5", "output")
-outputString = ""
+output = h5read("mpcOutput.hdf5", "output");
+outputString = "";
 
 for zone = 0:numZones-1
-    outputString = string(outputString, output[1 + zone*2], " ", output[2 + zone*2], " ")
+    outputString = string(outputString, output[1 + zone*2], " ", output[2 + zone*2], " ");
 end
 
-outputString = outputString[1:end-1]
-print(outputString)
+outputString = outputString[1:end-1];
+write(STDOUT, outputString);
